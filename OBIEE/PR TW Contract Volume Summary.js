85e6005fed8f3d5c466dcdc5513b0f8a51dea3bc
Tony Ren <renchk@foxmail.com>
2017-02-28 12:38:34 +0800

    Create PR TW Contract Volume Summary.js

---
+++ OBIEE/PR TW Contract Volume Summary.js
@@ -0,0 +1,40 @@
+</script>
+<div id="bulletc1"></div>
+<script>
+var margin = {top: 5, right: 40, bottom: 20, left: 120},
+    width = 960 - margin.left - margin.right,
+    height = 50 - margin.top - margin.bottom;
+
+var chart = d3.bullet()
+    .width(width)
+    .height(height);
+
+
+
+  var svg = d3.select("#bulletc1").selectAll("svg")
+      .data(data)
+    .enter().append("svg")
+      .attr("class", "bullet")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+      .call(chart);
+
+  var title = svg.append("g")
+      .style("text-anchor", "end")
+      .attr("transform", "translate(-6," + height / 2 + ")");
+
+  title.append("text")
+      .attr("class", "title")
+      .text(function(d) { return d.title; });
+
+  title.append("text")
+      .attr("class", "subtitle")
+      .attr("dy", "1em")
+      .text(function(d) { return d.subtitle; });
+
+
+
+
+</script>
